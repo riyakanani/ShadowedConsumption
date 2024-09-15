c74f73ff7ad6f0d0aeb9cc84f6e3f9841e6d0ece
Riya Kanani <rkanani@umd.edu>
2024-09-15 08:28:16 -0400

    Basic template

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private void Awake (){
+        body = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        
+        //flips player when moving left/right
+        if(horizontalInput > 0.01f){
+            transform.localScale = Vector3.one;
+        } else if(horizontalInput < -0.01f){
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        if(Input.GetKey(KeyCode.Space)){
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
+}
