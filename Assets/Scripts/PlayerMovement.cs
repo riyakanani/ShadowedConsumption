22767a47846b4b783bb06f7208c1f1f5bccf1794
Riya Kanani <rkanani@umd.edu>
2024-09-16 18:43:46 -0400

    added more animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -45,7 +45,7 @@ public class PlayerMovement : MonoBehaviour
                 body.gravityScale = 0;
                 body.velocity = Vector2.zero;
             } else {
-                body.gravityScale = 7;
+                body.gravityScale = 5;
             }
 
             if(Input.GetKey(KeyCode.Space)){
