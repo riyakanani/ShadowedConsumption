600e07cdacc283bbc41d007c06746f0bf6d37e58
Riya Kanani <rkanani@umd.edu>
2024-12-16 11:07:20 -0500

    fixed cam movement

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -6,15 +6,29 @@ using UnityEngine.UI;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private float currentPosX;
+    private float currentPosX, currentPosY;
     private Vector3 velocity = Vector3.zero;
+
+    // void Update()
+    // {
+    //     transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+    // }
 
     void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            new Vector3(currentPosX, currentPosY, transform.position.z),
+            ref velocity,
+            speed
+        );
     }
 
-    public void MoveToNewRoom(Transform _newRoom){
-        currentPosX = _newRoom.position.x;
+    public void MoveToNewRoomX(Transform _newRoom){
+        currentPosX = _newRoom.position.x; 
     }
+    public void MoveToNewRoomY(Transform _newRoom){
+        currentPosY = _newRoom.position.y; 
+    }
+
 }
--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -7,14 +7,69 @@ public class Door : MonoBehaviour
     [SerializeField] private Transform previousRoom;
     [SerializeField] private Transform nextRoom;
     [SerializeField] private CameraController cam;
-    
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.tag == "Player"){
-            if(collision.transform.position.x < transform.position.x){
-                cam.MoveToNewRoom(nextRoom);
-            } else {
-                cam.MoveToNewRoom(previousRoom);
-            }        
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Handle X-axis movement
+            if (collision.transform.position.x < transform.position.x)
+            {
+                cam.MoveToNewRoomX(nextRoom);
+            }
+            else
+            {
+                cam.MoveToNewRoomX(previousRoom);
+            }
+
+            // Additional logic for Room3 to handle Y-axis movement
+            if (nextRoom.name == "Room3")
+            {
+                if (collision.transform.position.y < transform.position.y)
+                {
+                    cam.MoveToNewRoomY(nextRoom);
+                }
+                else
+                {
+                    cam.MoveToNewRoomY(previousRoom);
+                }
+            }
         }
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Door : MonoBehaviour
+// {
+//     [SerializeField] private Transform previousRoom;
+//     [SerializeField] private Transform nextRoom;
+//     [SerializeField] private CameraController cam;
+    
+//     private void OnTriggerEnter2D(Collider2D collision){
+//         if(collision.tag == "Player"){
+//             if(collision.transform.position.x < transform.position.x){
+//                 cam.MoveToNewRoom(nextRoom);
+//             } else {
+//                 cam.MoveToNewRoom(previousRoom);
+//             } 
+
+//             if(nextRoom.name == "Room3"){
+//                 if(collision.transform.position.y < transform.position.y){
+//                     cam.MoveToNewRoomX(nextRoom);
+//                 } else {
+//                     cam.MoveToNewRoomY(previousRoom);
+//                 } 
+//             }
+//             // if(collision.transform.position.y < transform.position.y){
+//             //     cam.MoveToNewRoom(nextRoom);
+//             // } else {
+//             //     cam.MoveToNewRoom(previousRoom);
+//             // } 
+
+//         }
+//     }
+// }
