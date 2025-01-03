b71b40e43385ee7926d713d116988c0e4c271c97
Riya Kanani <rkanani@umd.edu>
2025-01-02 23:19:03 -0500

    shadowhand animation

--- Assets/Scripts/Enemies/Enemies_sideways.cs
+++ Assets/Scripts/Enemies/Enemies_sideways.cs
@@ -7,62 +7,82 @@ public class Enemies_sideways : MonoBehaviour
     [SerializeField] private float movementDistance;
     [SerializeField] private float speed;
     [SerializeField] private float damage = 20f;
+    [SerializeField] private float detectionRange = 15f; // Distance to detect the player
     private bool movingLeft;
     private float leftEdge;
     private float rightEdge;
-    // [SerializeField] private float range;
+    private Animator animator;
 
-    // [Header("Player Layer")]
-    // [SerializeField] private LayerMask playerLayer;
-    
-    // [Header("Collider Parameters")]
-    // [SerializeField] private float colliderDistance;
-    // [SerializeField] private BoxCollider2D boxCollider;
-
-    // [Header("SFX")]
-    // [SerializeField] private AudioClip handSound;
-
-    private void Awake(){
+    private void Awake()
+    {
         leftEdge = transform.position.x - movementDistance;
         rightEdge = transform.position.x + movementDistance;
+        animator = GetComponent<Animator>(); // Reference the Animator component
     }
 
-    private void Update(){
-        if(movingLeft){
-            if(transform.position.x > leftEdge){
+    private void Update()
+    {
+        HandleMovement();
+        DetectPlayerAndTriggerAnimation();
+    }
+
+    private void HandleMovement()
+    {
+        if (movingLeft)
+        {
+            if (transform.position.x > leftEdge)
+            {
                 transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
-            } else {
+            }
+            else
+            {
                 movingLeft = false;
             }
-        } else{
-            if(transform.position.x < rightEdge){
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
                 transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
-            } else {
+            }
+            else
+            {
                 movingLeft = true;
             }
-
         }
-
-        // if (PlayerInSight())
-        // {
-        //     SoundManager.instance.PlaySound(handSound);
-        //     Debug.Log("here");
-        // }
     }
 
-    // private bool PlayerInSight()
-    // {
-    //     RaycastHit2D hit =
-    //         Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
-    //         new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
-    //         0, Vector2.left, 0, playerLayer);
-
-    //     return hit.collider != null;
-    // }
-
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.tag == "Player"){
-            collision.GetComponent<Health>().TakeDamage(damage); 
+    private void DetectPlayerAndTriggerAnimation()
+{
+    GameObject player = GameObject.FindGameObjectWithTag("Player"); // Find the player
+    if (player != null)
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        // Debug.Log(distance);
+        if (distance <= detectionRange) //15f is the dection range;
+        {
+            if (!animator.GetBool("PlayerClose")) // Only update if there's a change
+            {
+                animator.SetBool("PlayerClose", true);
+            }
         }
-    }     
+        else
+        {
+            if (animator.GetBool("PlayerClose")) // Only update if there's a change
+            {
+                animator.SetBool("PlayerClose", false);
+                Debug.Log("PlayerClose: " + animator.GetBool("PlayerClose"));
+
+            }
+        }
+    }
+}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
 }
